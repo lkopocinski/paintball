b0d1641d194ff91da64f4b9663feeceb6ea75514
lkopocinski <lkopocinski@gmail.com>
2019-06-14 14:14:42 +0200

    Prepare graph

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 from collections import defaultdict
 
+from graph import BaseGraph
+
 
 def load_knowledge_source(file_path):
     ks_dict = defaultdict(list)
@@ -10,3 +12,10 @@ def load_knowledge_source(file_path):
             ks_dict[source].append((target, support))
 
     return ks_dict
+
+
+def prepare_graph(graph_path):
+    graph = BaseGraph()
+    graph.unpickle(graph_path)
+    graph.generate_lemma_to_nodes_dict_lexical_units()
+    return graph
