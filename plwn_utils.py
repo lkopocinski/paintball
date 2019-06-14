6f276cd940269a5181e35701ac29357ffa9bc7e5
lkopocinski <lkopocinski@gmail.com>
2019-06-14 15:13:56 +0200

    Setup initial activations

---
+++ plwn_utils.py
@@ -0,0 +1,14 @@
+import plwn
+
+
+class PLWN:
+
+    def __init__(self):
+        self.plwd = plwn.load_default()
+
+    def synset_len(self, synset_id):
+        try:
+            synset = self.plwd.synset_by_id(synset_id)
+            return len(synset.lexical_units)
+        except Exception:
+            return 1
--- utils.py
+++ utils.py
@@ -3,15 +3,32 @@ from collections import defaultdict
 from graph import BaseGraph
 
 
-def load_knowledge_source(file_path):
+def load_knowledge_source(path):
     ks_dict = defaultdict(list)
 
-    with open(file_path, 'r') as f:
+    with open(path, 'r') as f:
         for line in f:
             source, target, support = line.strip().split(';')
             ks_dict[source].append((target, support))
 
     return ks_dict
+
+
+def load_impedance_table(path):
+    it = defaultdict(lambda: defaultdict(float))
+
+    with open(path) as f:
+        headers = f.readline().strip().split(',')
+        headers = [h.strip() for h in headers]
+
+        for l in f:
+            l = l.strip().split(',')
+            row = l[0].strip()
+            it[row] = defaultdict(float)
+            for i in range(1, len(headers)):
+                it[row][headers[i]] = float(l[i])
+
+    return it
 
 
 def prepare_graph(graph_path):
