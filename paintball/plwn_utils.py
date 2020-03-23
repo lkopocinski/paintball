8bb1b7613a06ce905e8fd326a9cb480c48f9e205
Łukasz Kopociński <lkopocinski@gmail.com>
2020-03-23 16:45:10 +0100

    refactor plwn_utils

--- paintball/plwn_utils.py
+++ paintball/plwn_utils.py
@@ -1,21 +1,22 @@
+from typing import Tuple
+
 import plwn
+from plwn.exceptions import SynsetNotFound
 
 
 class PLWN:
 
     def __init__(self):
-        self.plwd = plwn.load_default()
+        self._plwd = plwn.load_default()
 
-    def synset_len(self, synset_id):
-        try:
-            synset = self.plwd.synset_by_id(synset_id)
-            return len(synset.lexical_units)
-        except Exception:
-            return 1
+    def synset_len(self, synset_id: int) -> int:
+        lexical_units = self.synset_lexical_units(synset_id)
+        return len(lexical_units)
 
-    def synset_lexical_units(self, synset_id):
+    def synset_lexical_units(self, synset_id: int) -> Tuple:
         try:
-            synset = self.plwd.synset_by_id(synset_id)
+            synset = self._plwd.synset_by_id(synset_id)
+        except SynsetNotFound:
+            return tuple()
+        else:
             return synset.lexical_units
-        except Exception:
-            return []
