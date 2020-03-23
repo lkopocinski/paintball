02b3d71cf5a4775dae46e5b2346013fb672457eb
Łukasz Kopociński <lkopocinski@gmail.com>
2020-03-23 15:31:21 +0100

    Structure resources

--- res/remove_duplicates.py
+++
@@ -1,8 +0,0 @@
-with open("/mnt/tbstorage/lukasz/data_set/paint_ball/poli_mono_select.txt.csv_sim.csv") as f:
-    for line in f:
-        line = line.strip()
-        s, t, w = line.split(';')
-
-        if s != t:
-            print line
-
